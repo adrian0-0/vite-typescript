c9dddc2c2ce8bba2a5b5087b535c63350633f778
Adrian Vinicius <adrianromaalves@hotmail.com>
2023-10-20 12:40:53 -0300

    forms

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useState } from "react";
 // import "module-alias/register";
 // import { User } from "@src";
-import reactLogo from "./assets/react.svg";
+// import reactLogo from "./assets/react.svg";
 
-import viteLogo from "../public/vite.svg";
+// import viteLogo from "../public/vite.svg";
 import "./App.css";
 
 function App() {
@@ -13,6 +13,33 @@ function App() {
   return (
     <div>
       <div>
+        <h1>Contact Form</h1>
+        <form
+          action="https://formsubmit.co/2007a25874ff79ac6467c7a31d68cea1"
+          method="POST"
+          encType="multipart/form-data"
+          git
+          s
+        >
+          <input type="text" name="Name" placeholder=" Nome Completo"></input>
+          <input type="email" name="Email" placeholder="Email" required></input>
+          <select name="Continente" id="">
+            <option value="Africa"></option>
+            <option value="Antartica"></option>
+            <option value="Asia"></option>
+            <option value="Oceania"></option>
+            <option value="Europa"></option>
+            <option value="America"></option>
+          </select>
+          <textarea name="Message" placeholder="Mensagem" required></textarea>
+          <label htmlFor="documento">Selecione um documento:</label>
+          <input type="file" name="documento" id="documento"></input>
+          <input type="file" name="documento" id="documento"></input>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,7 +58,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </div>
   );
 }
